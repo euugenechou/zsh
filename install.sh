1cecf791fac04ac917b0f794695d61e257871b32
Eugene Chou <euchou@ucsc.edu>
2023-03-10 02:28:49 -0800

    oops

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 # Install oh-my-zsh.
-if ! test -d "$HOME/.oh-my-zsh"; then
+if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
 
 # Install zplug.
-git clone "https://github.com/zplug/zplug" "$HOME/.zplug"
+if [[ ! -d "$HOME/.zplug" ]]; then
+    git clone "https://github.com/zplug/zplug" "$HOME/.zplug"
+fi
 
 # Relink configuration.
-rm -f "$PWD/.zshrc"
-rm -f "$PWD/milktea.zsh-theme"
+rm -f "$HOME/.zshrc"
+rm -f "$HOME/.oh-my-zsh/themes/milktea.zsh-theme"
 ln -sf "$PWD/.zshrc" "$HOME/.zshrc"
 ln -sf "$PWD/milktea.zsh-theme" "$HOME/.oh-my-zsh/themes/milktea.zsh-theme"
